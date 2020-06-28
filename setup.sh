893413177671fc459c50e89309bb11d92d39eaf2
suren <suren>
2020-06-28 21:59:34 +0100

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
-printf "\n..CW App Setup..\n"
+printf "\n..CW App Setup.."
+printf "\n..Make sure you're already logged into Docker Hub via Docker login..\n\n"
 
 username=""
 
-read -p "Enter your Docker username. Make sure you're already logged in\n"
+read -p "Enter your Docker Hub username: "
 username=$REPLY
 
 printf "\n..Building v1.0..\n"
@@ -15,7 +16,9 @@ docker build ./v2.0/ -t $username/cw-app:2.0
 printf "\n..Building v2.1..\n"
 docker build ./v2.1/ -t $username/cw-app:2.1
 
-printf "\n..Pushing Images to Docker Hub"
+printf "\n..Pushing Images to Docker Hub..\n"
 docker push $username/cw-app:1.0
 docker push $username/cw-app:2.0
 docker push $username/cw-app:2.1
+
+printf "\n...Done...\n"
